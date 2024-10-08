975752d04658f138dfea3b9d23ad6c607550284c
tomikartemik <artemules@mail.ru>
2024-10-09 00:07:33 +0300

    FIX: cart

--- pkg/repository/cart.go
+++ pkg/repository/cart.go
@@ -14,36 +14,36 @@ func NewCartRepository(db *gorm.DB) *CartRepository {
 }
 
 func (r *CartRepository) AddToCart(cartItem model.CartItem) error {
-	return r.db.Create(&cartItem).Error
+	return r.db.Model(&model.CartItem{}).Create(&cartItem).Error
 }
 
 func (r *CartRepository) GetCartByBuyerID(buyerID string) (model.Cart, error) {
 	var cart model.Cart
-	err := r.db.Preload("CartItems.Item").Where("buyer_id = ?", buyerID).Find(&cart).Error
+	err := r.db.Model(&model.CartItem{}).Preload("CartItems.Item").Where("buyer_id = ?", buyerID).Find(&cart).Error
 	return cart, err
 }
 
 func (r *CartRepository) UpdateCartItem(cartItem model.CartItem) error {
-	return r.db.Save(&cartItem).Error
+	return r.db.Model(&model.CartItem{}).Save(&cartItem).Error
 }
 
 func (r *CartRepository) GetCartItemByID(cartItemID int) (model.CartItem, error) {
 	var cartItem model.CartItem
-	err := r.db.Where("id = ?", cartItemID).First(&cartItem).Error
+	err := r.db.Model(&model.CartItem{}).Where("id = ?", cartItemID).First(&cartItem).Error
 	return cartItem, err
 }
 
 func (r *CartRepository) RemoveFromCart(cartItemID int) error {
-	return r.db.Delete(&model.CartItem{}, cartItemID).Error
+	return r.db.Model(&model.CartItem{}).Delete(&model.CartItem{}, cartItemID).Error
 }
 
 func (r *CartRepository) ClearCart(buyerID string) error {
-	return r.db.Where("buyer_id = ?", buyerID).Delete(&model.CartItem{}).Error
+	return r.db.Model(&model.CartItem{}).Where("buyer_id = ?", buyerID).Delete(&model.CartItem{}).Error
 }
 
 func (r *CartRepository) GetCartItemsByBuyerID(buyerID string) ([]model.CartItem, error) {
 	var cartItems []model.CartItem
-	if err := r.db.Where("buyer_id = ?", buyerID).Find(&cartItems).Error; err != nil {
+	if err := r.db.Model(&model.CartItem{}).Where("buyer_id = ?", buyerID).Find(&cartItems).Error; err != nil {
 		return nil, err
 	}
 	return cartItems, nil
